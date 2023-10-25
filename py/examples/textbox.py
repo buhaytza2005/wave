065681a252bedd19eec459926ff13935ad30a880
buhaytza2005 <buhaytza2005@gmail.com>
2023-10-25 14:11:04 +0100

    chore: fix examples

--- py/examples/textbox.py
+++ py/examples/textbox.py
@@ -22,7 +22,8 @@ async def serve(q: Q):
             ui.text(f'textbox_disabled_placeholder={q.args.textbox_disabled_placeholder}'),
             ui.text(f'textbox_multiline={q.args.textbox_multiline}'),
             ui.text(f'textbox_spellcheck_disabled={q.args.textbox_spellcheck_disabled}'),
-            ui.text(f'textbox_spellcheck_disabled={q.args.textbox_spellcheck_disabled}'),
+            ui.text(f'textbox_type_number={q.args.textbox_type_number}'),
+            ui.text(f'textbox_type_tel={q.args.textbox_type_tel}'),
             ui.button(name='show_form', label='Back', primary=True),
         ]
     else:
@@ -41,8 +42,8 @@ async def serve(q: Q):
                        placeholder='I am disabled'),
             ui.textbox(name='textbox_multiline', label='Multiline textarea', multiline=True),
             ui.textbox(name='textbox_spellcheck_disabled', label='Spellcheck disabled', spellcheck=False),
-            ui.textbox(name='textbox_type_number', label='Textbox with type number', type='number', spellcheck=False),
-            ui.textbox(name='textbox_type_tel', label='Textbox with type tel', type='tel', spellcheck=False),
+            ui.textbox(name='textbox_type_number', label='With type number', type='number', spellcheck=False),
+            ui.textbox(name='textbox_type_tel', label='With type tel', type='tel', spellcheck=False),
             ui.button(name='show_inputs', label='Submit', primary=True),
         ])
     await q.page.save()
